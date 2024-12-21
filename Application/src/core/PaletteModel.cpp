fc2ad10201bf5578420dc139dd1b77bd642a7bd9
DESKTOP-G2EVAU9\admin <vunh@hiptechvn.com>
2024-12-21 23:08:48 +0700

    add color model for palette

--- Application/src/core/PaletteModel.cpp
+++ Application/src/core/PaletteModel.cpp
@@ -3,10 +3,103 @@
 #include <mutex>
 #include <optional>
 
+namespace {
+
 std::once_flag flag;
 std::optional<QColor> m_enhanceRequest;
 static PaletteModel *model = nullptr;
 
+const int singleColorRange = 7;
+
+QVector<QColor> rgbDefaultColor = {
+    {"#fe4a49"},
+    {"#2ab7ca"},
+    {"#fed766"},
+    {"#f4b6c2"},
+    {"#6497b1"},
+    {"#451e3e"},
+    {"#35a79c"},
+    {"#dfa290"},
+    {"#3d1e6d"},
+    {"#b8860b"},
+    {"#f37736"},
+    {"#556b2f"},
+    {"#2f4f4f"},
+    {"#e0ffff"}
+};
+
+QVector<QColor> getRangeOfSingleColor(const QColor &color)
+{
+    QVector<QColor> colors;
+    const auto rangeColor = singleColorRange - 1;
+    for (int i = 0; i < rangeColor; i++) {
+        const auto darkerColor = color.darker(rangeColor * 100 / (rangeColor - i));
+        colors.append(darkerColor);
+    }
+    colors.append(color);
+    return colors;
+}
+
+}
+
+ColorModel::UPtr ColorModel::getRgbColorModelInstance()
+{
+    return UPtr(new ColorModel());
+}
+
+ColorModel::UPtr ColorModel::getSingleColorModelInstance(const QColor &color)
+{
+    return UPtr(new ColorModel(getRangeOfSingleColor(color)));
+}
+
+QVariant ColorModel::data(const QModelIndex &index, int role) const
+{
+    if (!index.isValid()) {
+        Q_ASSERT(false);
+        return {};
+    }
+
+    if (index.row() < 0 || index.row() > m_colors.size()) {
+        Q_ASSERT(false);
+        return {};
+    }
+
+    switch(role) {
+    default: {
+        Q_ASSERT(false);
+        return {};
+    }
+    case ColorRole: {
+        return QVariant::fromValue(m_colors[index.row()]);
+    }
+    }
+}
+
+int ColorModel::rowCount(const QModelIndex &) const
+{
+    return m_colors.size();
+}
+
+QHash<int, QByteArray> ColorModel::roleNames() const
+{
+    return {
+        {ColorRole, "colors"}
+    };
+}
+
+ColorModel::ColorModel()
+{
+}
+
+ColorModel::ColorModel(const QVector<QColor> &colors)
+    : m_colors(std::move(colors))
+{
+}
+
+void ColorModel::setSingleColor(const QColor &)
+{
+
+}
 
 PaletteModel *PaletteModel::instantiateQmlSingleton(QQmlEngine *engine, QJSEngine *jsEngine)
 {
@@ -22,6 +115,12 @@ PaletteModel *PaletteModel::instantiateQmlSingleton(QQmlEngine *engine, QJSEngin
 
 PaletteModel::PaletteModel(QObject *parent) : QObject(parent)
 {
+}
+
+void PaletteModel::initColorModel()
+{
+    m_rgbColorModel = ColorModel::getRgbColorModelInstance();
+    m_singleColorModel = ColorModel::getSingleColorModelInstance(Qt::white);
 }
 
 QColor PaletteModel::textColor() const
@@ -52,4 +151,28 @@ void PaletteModel::setBackgroundColor(QColor)
 void PaletteModel::setAccentColor(QColor)
 {
 
+}
+
+QObject *PaletteModel::singleColorModel() const
+{
+    return m_singleColorModel.get();
+}
+
+QObject *PaletteModel::rgbColorModel() const
+{
+    return m_rgbColorModel.get();
+}
+
+bool PaletteModel::singleColor() const
+{
+    return m_isSingleColor;
+}
+
+void PaletteModel::setSingleColor(bool value)
+{
+    if (m_isSingleColor == value) {
+        return;
+    }
+    m_isSingleColor = value;
+    emit singleColorChanged();
 }
--- Application/src/core/PaletteModel.h
+++ Application/src/core/PaletteModel.h
@@ -4,6 +4,36 @@
 #include <QColor>
 #include <QQmlEngine>
 #include <QJSEngine>
+#include <QAbstractListModel>
+
+class ColorModel final : public QAbstractListModel
+{
+    Q_OBJECT
+public:
+    using UPtr = std::unique_ptr<ColorModel>;
+
+    ColorModel (const ColorModel &) = delete;
+    ColorModel& operator=(const ColorModel &) = delete;
+
+    enum Roles {
+        ColorRole = Qt::UserRole + 1
+    };
+    Q_ENUM(Roles)
+
+    static UPtr getRgbColorModelInstance();
+    static UPtr getSingleColorModelInstance(const QColor &baseColor);
+
+    QVariant data(const QModelIndex &index, int role) const final;
+    int rowCount(const QModelIndex &parent = {}) const final;
+    QHash<int, QByteArray> roleNames() const final;
+
+    void setSingleColor(const QColor &);
+
+private:
+    explicit ColorModel();
+    explicit ColorModel(const QVector<QColor> &);
+    QVector<QColor> m_colors;
+};
 
 class PaletteModel : public QObject
 {
@@ -12,6 +42,9 @@ class PaletteModel : public QObject
     Q_PROPERTY(QColor textColor READ textColor WRITE setTextColor NOTIFY textColorChanged)
     Q_PROPERTY(QColor backgroundColor READ backgroundColor WRITE setBackgroundColor NOTIFY backgroundChanged)
     Q_PROPERTY(QColor accentColor READ accentColor WRITE setAccentColor NOTIFY accentColorChanged)
+    Q_PROPERTY(bool singleColor READ singleColor WRITE setSingleColor NOTIFY singleColorChanged)
+    Q_PROPERTY(QObject *rgbColorModel READ rgbColorModel CONSTANT)
+    Q_PROPERTY(QObject *singleColorModel READ singleColorModel CONSTANT)
 
 public:
     PaletteModel(const PaletteModel &) = delete;
@@ -23,20 +56,32 @@ public:
     QColor textColor() const;
     QColor backgroundColor() const;
     QColor accentColor() const;
+    bool singleColor() const;
 
     void setTextColor(QColor);
     void setBackgroundColor(QColor);
     void setAccentColor(QColor);
+    void setSingleColor(bool);
+
+    QObject *singleColorModel() const;
+    QObject *rgbColorModel() const;
 
 signals:
     void textColorChanged();
     void backgroundChanged();
     void accentColorChanged();
+    void singleColorChanged();
 
 private:
     PaletteModel(QObject *parent = nullptr);
+    void initColorModel();
 
     QColor m_textColor;
     QColor m_backgroundColor;
     QColor m_accentColor;
+
+    ColorModel::UPtr m_rgbColorModel;
+    ColorModel::UPtr m_singleColorModel;
+
+    bool m_isSingleColor = false;
 };
